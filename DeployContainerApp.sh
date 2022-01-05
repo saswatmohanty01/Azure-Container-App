5482cd5635ebb427609a95495ddb126e5d4349d6
khanasif1 <khanasif1@gmail.com>
2022-01-05 16:23:38 +1100

    bash for Container App create

---
+++ DeployContainerApp.sh
@@ -0,0 +1,43 @@
+az login
+
+#Register the Azure Container Registry
+az extension add --source https://workerappscliextension.blob.core.windows.net/azure-cli-extension/containerapp-0.2.0-py2.py3-none-any.whl
+
+az provider register --namespace Microsoft.Web
+
+# Variables
+
+RESOURCE_GROUP="my-container-apps"
+LOCATION="canadacentral"
+LOG_ANALYTICS_WORKSPACE="my-container-apps-logs"
+CONTAINERAPPS_ENVIRONMENT="my-environment"
+
+#Monitoring for Azure Container Apps
+az monitor log-analytics workspace create \
+  --resource-group $RESOURCE_GROUP \
+  --workspace-name $LOG_ANALYTICS_WORKSPACE
+
+#Log Analytics Workspace details
+LOG_ANALYTICS_WORKSPACE_CLIENT_ID=`az monitor log-analytics workspace show --query customerId -g $RESOURCE_GROUP -n $LOG_ANALYTICS_WORKSPACE -o tsv | tr -d '[:space:]'`
+
+LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET=`az monitor log-analytics workspace get-shared-keys --query primarySharedKey -g $RESOURCE_GROUP -n $LOG_ANALYTICS_WORKSPACE -o tsv | tr -d '[:space:]'`
+
+
+#Azure Container Apps Environment 
+az containerapp env create \
+  --name $CONTAINERAPPS_ENVIRONMENT \
+  --resource-group $RESOURCE_GROUP \
+  --logs-workspace-id $LOG_ANALYTICS_WORKSPACE_CLIENT_ID \
+  --logs-workspace-key $LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET \
+  --location "$LOCATION"
+
+#Azure Container Apps
+
+  az containerapp create \
+  --name my-container-app \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image mcr.microsoft.com/azuredocs/containerapps-helloworld:latest \
+  --target-port 80 \
+  --ingress 'external' \
+  --query configuration.ingress.fqdn
